484b81f485e64842081d5689b00e4c5b44a459e5
Kevin Chang <kevincchang13@gmail.com>
2017-11-02 17:21:26 -0700

    saved files

--- collatzSequence.js
+++ collatzSequence.js
@@ -20,3 +20,37 @@
 // collatzSequence(7); // [7, 22, 11, 34, 17, 52, 26, 13, 40, 20, 10, 5, 16, 8, 4, 2, 1]
 // collatzSequence(0); // "Input must be a positive whole number."
 // collatzSequence([]); // "Input must be a positive whole number."
+
+//Iteration
+function collatzSequence(n) {
+    if (n === null || n === 0) {
+        return "Input must be a positive whole number."
+    }
+    var answer = [n]
+    var temp = n
+    while (temp !== 1) {
+        if (temp%2 === 0) {
+            temp = temp/2
+        } else if (temp%2 === 1) {
+            temp = temp*3+1
+        }
+        answer.push(temp)
+    }
+    answer.push(1)
+    return answer
+}
+
+//Recursive
+function collatzSequence(n, answer=[]) {
+    if (n === null || n === 0) return "Input must be a positive whole number."
+    if (n === 1) {
+        answer.push(n)
+        return answer
+    }
+    answer.push(n)
+    if (n%2 === 0) {
+        collatzSequence(n/2, answer)
+    } else {
+        collatzSequence((n*2+1), answer)
+    }
+}
--- hammingDistance-oneCharDifference.js
+++ hammingDistance-oneCharDifference.js
@@ -47,5 +47,26 @@ function hammingDistance(str1, str2) {
 
 
 function oneCharDifference(str1, str2) {
+    let count = 0
+    let i = 0
+    let j = 0
 
+    while (j < str1.length) {
+        if (str1[i] === str2[j]) {
+            i++
+            j++
+        } else if (str1[i] !== str2[j] && str1[i+1] === str2[j+1]) {
+            count++
+            j++
+            i++
+        } else if(str1[i] !== str2[j] && str1[i] === str2[j+1]) {
+            count++
+            j++
+        }
+    }
+    if (count === 1) {
+        return true
+    } else {
+        return false
+    }
 }
